1ce52d2cbaa20fecd6ac40a40ff40587f3d9f16f
RuLe0G <khlebnikov2015@yandex.ru>
2020-05-29 20:37:05 +0700

    Part 1

---
+++ 1/1/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace _1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoverButton1 = new _1.HoverButton();
+            this.hoverButtonEllipse1 = new _1.HoverButtonEllipse();
+            this.SuspendLayout();
+            // 
+            // hoverButton1
+            // 
+            this.hoverButton1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButton1.ForeColor = System.Drawing.Color.White;
+            this.hoverButton1.Location = new System.Drawing.Point(13, 12);
+            this.hoverButton1.Name = "hoverButton1";
+            this.hoverButton1.Size = new System.Drawing.Size(382, 426);
+            this.hoverButton1.TabIndex = 2;
+            this.hoverButton1.Text = "hoverButton1";
+            this.hoverButton1.UseVisualStyleBackColor = true;
+            this.hoverButton1.Click += new System.EventHandler(this.hoverButton1_Click);
+            // 
+            // hoverButtonEllipse1
+            // 
+            this.hoverButtonEllipse1.Font = new System.Drawing.Font("Microsoft YaHei UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoverButtonEllipse1.ForeColor = System.Drawing.Color.White;
+            this.hoverButtonEllipse1.Location = new System.Drawing.Point(411, 12);
+            this.hoverButtonEllipse1.Name = "hoverButtonEllipse1";
+            this.hoverButtonEllipse1.Size = new System.Drawing.Size(377, 426);
+            this.hoverButtonEllipse1.TabIndex = 1;
+            this.hoverButtonEllipse1.Text = "hoverButtonEllipse1";
+            this.hoverButtonEllipse1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.hoverButton1);
+            this.Controls.Add(this.hoverButtonEllipse1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HoverButtonEllipse hoverButtonEllipse1;
+        private HoverButton hoverButton1;
+    }
+}
+
---
+++ 1/1/HoverButton.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Resources;
+using _1.Properties;
+
+namespace _1
+{
+    class HoverButton : Button
+    {
+        private Color color = Color.SkyBlue;
+        private int i = 1;
+        private string txt;
+        private StringFormat stringFormat;
+        private bool txtb = false;
+
+        public HoverButton() : base()
+        {
+            ForeColor = Color.White;
+            Font = new Font("Microsoft YaHei UI",
+             20.25F,
+             FontStyle.Bold,
+             GraphicsUnit.Point,
+
+             0);
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+            txtb = true;
+        }
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            if (i == 7)
+                i = 0;
+            i += 1;
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+            txtb = false;
+        }
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+        }
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            // получение картинки из ресурсов
+            Bitmap bitmap = null;
+
+            switch (i)
+            {
+                case 1:
+                    bitmap = new Bitmap(Resources.Image1);
+                    txt = "Картинка 1";
+                    break;
+                case 2:
+                    bitmap = new Bitmap(Resources.Image2);
+                    txt = "Картинка 2";
+                    break;
+                case 3:
+                    bitmap = new Bitmap(Resources.Image3);
+                    txt = "Картинка 3";
+                    break;
+                case 4:
+                    bitmap = new Bitmap(Resources.Image4);
+                    txt = "Картинка 4";
+                    break;
+                case 5:
+                    bitmap = new Bitmap(Resources.Image5);
+                    txt = "Картинка 5";
+                    break;
+                case 6:
+                    bitmap = new Bitmap(Resources.Image6);
+                    txt = "Картинка 6";
+                    break;
+                case 7:
+                    bitmap = new Bitmap(Resources.Image7);
+                    txt = "Картинка 7";
+                    break;
+            }
+            if (txtb == false)
+            {
+                txt = "";
+            }
+            // отрисовка картинки в точке (0,0)
+            pe.Graphics.DrawImage(bitmap, 0, 0, Size.Width, Size.Height);
+
+            stringFormat = new StringFormat();
+            stringFormat.Alignment = StringAlignment.Center;
+            stringFormat.LineAlignment = StringAlignment.Center;
+
+
+            pe.Graphics.DrawString(txt, Font, Brushes.Aqua, ClientRectangle, stringFormat);
+
+        }
+
+    }
+}
